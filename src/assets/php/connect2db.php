78a71a340fb67624143372be2dfb7379a56021b4
Krauseandy <andykrausepro@gmail.com>
2018-11-27 16:06:11 +0100

    modif

--- src/assets/php/connect2db.php
+++ src/assets/php/connect2db.php
@@ -3,12 +3,17 @@
 date_default_timezone_set("Europe/Brussels");
 
 
-    //CONNEXION A LA BASE DE DONNEES
-    try {
-        $connexion = new PDO("mysql:host=mysql;dbname=messenger", "messenger", "messenger");
-        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo("Connected successfully");
-    } catch (PDOException $e) {
-        echo("Connection failed".$e->getMessage());
-    }
+
+try {
+    
+    $timezone = date_default_timezone_get();
+    $connexion = new PDO("mysql:host=mysql;dbname=messenger", "messenger", "messenger");
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexion->exec("SET time_zone = '{$timezone}'");
+} catch (PDOException $e) {
+    echo("Connection failed".$e->getMessage());
+}
+
+
+
 ?>
