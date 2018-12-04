c6aefdc72c94833e31228c4cfae00072cb427999
badouuuuuu <el.hirech.youssef@gmail.com>
2018-12-04 15:56:37 +0100

    sent

--- src/post-message.php
+++ src/post-message.php
@@ -16,6 +16,7 @@ if(isset($_POST['send-message'])) {
         $stmt->bindValue(':convers', $convers);
         $stmt->bindValue(':content', $text_message);    
         $stmt->execute();
+       
     }
 
     else {
