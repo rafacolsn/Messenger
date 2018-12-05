76bb70e4e0ad65a002b9099e9823db386464525c
badouuuuuu <el.hirech.youssef@gmail.com>
2018-12-05 10:44:46 +0100

    add members to participation table

--- src/assets/php/connect2db.php
+++ src/assets/php/connect2db.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("Europe/Brussels");
 try {
     $timezone = date_default_timezone_get();
-    $connexion = new PDO("mysql:host=mysql;dbname=messenger", "messenger", "messenger");
+    $connexion = new PDO("mysql:host=mysql-raphcolson.alwaysdata.net;dbname=raphcolson_messenger", "172648", "MDP?Msger");
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $connexion->exec("SET time_zone = '{$timezone}'");
 } catch (PDOException $e) {
