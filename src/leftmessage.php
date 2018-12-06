f54ddf8d2621a938eeb18880305518ff9df4fc58
rafacolsn <raphcolson@gmail.com>
2018-12-06 09:03:32 +0100

    css notifs

--- src/leftmessage.php
+++ src/leftmessage.php
@@ -2,7 +2,6 @@
     <?php     
     session_start();
     require "./assets/php/connect2db.php";
-    
 
     $get_unread_topics = $connexion->prepare("SELECT 
                                         c.id_conversation, 
@@ -17,8 +16,6 @@
     $get_unread_topics->execute();
     
     $u_id=$_SESSION['user_id'];
-    
-
 
     while($datatopic = $get_unread_topics->fetch()) {
 
@@ -30,36 +27,39 @@
                 </a> 
 
                 <a href="messenger.php?cv_id='.$datatopic['id_conversation'].'&cv_name='.$datatopic['topicname'].'">
-                    <li class="topicleft" name="topicname">'.$datatopic['topicname'].'
-                        <div class="notifs">'.$datatopic['unread_msg'].'</div>
-                    </li>
+                    <li class="topicleft" name="topicname">'.$datatopic['topicname'];
+                        if ($datatopic['unread_msg'] > 0) {
+                            echo '<div class="notifs">'.$datatopic['unread_msg'].'</div>';
+                        }
+                 echo        
+                    '</li>
                 </a>';
         }
     };
 
-    $get_other_topics = $connexion->prepare("SELECT 
-                                                c.id_conversation, 
-                                                c.subject AS topicname, 
-                                                c.author_id, 
-                                                p.unread_msg, 
-                                                p.user_id AS u_id 
-                                                FROM T_CONVERSATION c 
-                                                INNER JOIN T_PARTICIPATION_CONVERSATION p 
-                                                ON c.id_conversation = p.conversation_id 
-                                                GROUP BY c.id_conversation");
-    $get_other_topics->execute();
+    // $get_other_topics = $connexion->prepare("SELECT 
+    //                                             c.id_conversation, 
+    //                                             c.subject AS topicname, 
+    //                                             c.author_id, 
+    //                                             p.unread_msg, 
+    //                                             p.user_id AS u_id 
+    //                                             FROM T_CONVERSATION c 
+    //                                             INNER JOIN T_PARTICIPATION_CONVERSATION p 
+    //                                             ON c.id_conversation = p.conversation_id 
+    //                                             GROUP BY c.id_conversation");
+    // $get_other_topics->execute();
 
-    while ($other_topics = $get_other_topics->fetch()) {
-        echo '
-            <br>
-            <a href="delete-message.php?action=delete_conv&id='.$other_topics['id_conversation'].'">
-                <i class="fa fa-close font-size:12px"></i>
-            </a> 
+    // while ($other_topics = $get_other_topics->fetch()) {
+    //     echo '
+    //         <br>
+    //         <a href="delete-message.php?action=delete_conv&id='.$other_topics['id_conversation'].'">
+    //             <i class="fa fa-close font-size:12px"></i>
+    //         </a> 
 
-            <a href="messenger.php?cv_id='.$other_topics['id_conversation'].'&cv_name='.$other_topics['topicname'].'">
-                <li class="topicleft" name="topicname">'.$other_topics['topicname'].'</li>
-            </a>';
-    }
+    //         <a href="messenger.php?cv_id='.$other_topics['id_conversation'].'&cv_name='.$other_topics['topicname'].'">
+    //             <li class="topicleft" name="topicname">'.$other_topics['topicname'].'</li>
+    //         </a>';
+    // }
 
     $_SESSION['cv_name'] = $_GET['cv_name'];
     $get_unread_topics->closeCursor();
