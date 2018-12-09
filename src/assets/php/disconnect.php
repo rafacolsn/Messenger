559f5e6f49664ab356bb5842d807fc31ce844dd2
PironSteve <steve.piron@gmail.com>
2018-12-09 14:45:48 +0100

    modification css profile/login/register

--- src/assets/php/disconnect.php
+++ src/assets/php/disconnect.php
@@ -5,12 +5,25 @@ if(isset($_POST['disconnect'])) { // Au clique sur le boutton "Deconnexion", sup
    $_SESSION = array();
 
 unset($_COOKIE['username']); // Delete cookies de l'user
-
-echo "<br> <h3 class='topic-title-left'>Vous avez été deconnecté de BigChat</h3> <br>
-<p> Cliquez <a href='http://localhost:8000/index.php'>ici</a> pour vous reconnecter </p>
-
-";
 session_destroy(); // Destruction de la session connectée
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Déconnexion de BigChat</title>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <link rel="stylesheet" href="../assets/css/disconnect.css">
+</head>
+<body>
+   <div class="title-page">
+      <h2>Vous avez été déconnecté de BigChat</h2>
+   </div>
+      <a href="http://localhost:8000/index.php"><button class="btn btn-primary btn-lg center">Cliquez ici pour vous reconnecter</button></a>
+   </div>
+</body>
+</html>
